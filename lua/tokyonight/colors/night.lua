a49e642dd45e1d5ccd258f634504e8a0e2840014
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-09-30 19:32:07 +0530

    (feat) Update colors

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,6 +2,6 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#1f202d",
-  bg_dark = "#1b1b25",
+  bg = "#282836",
+  bg_dark = "#1c1c27",
 })
