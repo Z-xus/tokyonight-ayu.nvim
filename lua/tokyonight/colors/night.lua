4278638ceaf6d81a34a6f3036e338a7d97a39252
Naufil (Z-xus) <naufilasar.dev@gmail.com>
2024-09-30 19:30:27 +0530

    (feat) Add darker colors

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,6 +2,6 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#1a1b26",
-  bg_dark = "#16161e",
+  bg = "#1f202d",
+  bg_dark = "#1b1b25",
 })
